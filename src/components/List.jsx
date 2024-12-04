bab5eba2e91c2dfc8a4a260d2dbad2b6536a1089
Jordi Galobart Salvat <jgalobart@gmail.com>
2024-12-04 09:39:31 +0100

    get tasks from backend

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,30 +1,14 @@
 import Item from "./Item";
+import { useEffect, useState } from "react";
 
 function List() {
-  const tasks = [
-    {
-      description: "Preparar la clase de Nuclio Full Stack Developer",
-      status: "IN PROGRESS",
-      dueDate: "25 NOV 2024",
-    },
-    {
-      description:
-        "Devolver libros a la biblioteca y recoger nuevos libros sobre ciencia ficción",
-      status: "DONE",
-      dueDate: "04 DIC 2024",
-    },
-    {
-      description:
-        "Preparar las maletas y contratar el seguro de viaje a Marte",
-      status: "TO DO",
-      dueDate: "03 MAY 2025",
-    },
-    {
-      description: "Desarrollar un robot para que pueda programar por mi",
-      status: "TO DO",
-      dueDate: "25 DIC 2027",
-    },
-  ];
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
+  }, []);
 
   return (
     <>
